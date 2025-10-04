e17ae492a63ac8a092ab099c033223e705a3a371
MazenSehsah <mazenmazen.mmm44@gmail.com>
2025-10-04 16:46:05 +0300

    1st task with airflow

--- dags/dag1.py
+++ dags/dag1.py
@@ -1,24 +1,19 @@
 from airflow import DAG
+import random
 from datetime import datetime, timedelta
-from airflow.operators.python_operator import PythonOperator 
-def say_hello():
-    print("Hello Airflow !")
-def show_time():
-    print("Current time is:", datetime.now())
-
+from airflow.operators.python_operator import PythonOperator
+def random_number():
+    print(random.randint(0,1000))
 with DAG (
-    dag_id = "dag1",
-    start_date = datetime(2025, 9, 29),
+    dag_id = "Airflow_Depi",
+    start_date = datetime(2025, 10, 4),
     schedule_interval = timedelta(minutes= 1),
     catchup = False
 ) as dag :
-    task_say = PythonOperator(
-        task_id = "say_hello",
-        python_callable = say_hello
+    task3 = PythonOperator(
+         task_id = "Random_Number",
+         python_callable = random_number
     )
-    task2 = PythonOperator(
-        task_id = "time_task",
-        python_callable = show_time
-    )
+         
     
     
